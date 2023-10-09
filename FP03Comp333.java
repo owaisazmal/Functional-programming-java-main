9a5ab7c1dcf2c8957e5fa1e89cb6d49abd39efc7
Owais <owais.khan.244@my.csun.edu>
2023-10-08 17:03:48 -0700

    Comp 333

--- FP03Comp333.java
+++ FP03Comp333.java
@@ -3,7 +3,8 @@ import java.util.stream.IntStream;
 public class FP03Comp333 {
     public static void main(String[] args) {
     //System.out.println(question1(70));
-    System.out.println(question2(2));
+    //System.out.println(question2(2));
+    System.out.println(question3(2));
 
     }
 
@@ -22,7 +23,13 @@ public class FP03Comp333 {
     static double question3(int n){
         return IntStream
         .range(1, n+1)
-        .mapToDouble(x -> (3/x) +x)
+        .mapToDouble(x -> (double)(3/x) + (double)x)
+        .sum();
+    }
+    static double question4(int n){
+        return IntStream
+        .range(1, n+1)
+        .mapToDouble(x -> (double)x)
         .sum();
     }
 }
