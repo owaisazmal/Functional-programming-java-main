1066eb94ce06519882c9b2c47da83366fd3b901e
Owais <owais.khan.244@my.csun.edu>
2023-11-08 22:41:37 -0800

    Threadpooling intro

---
+++ factorialArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class factorialArray implement runnable {
+    int i;
+    Long[]result;
+}
+factorialArray(long[]result, int i){
+
+This.i = i;
+This.result = result;
+
+}
+
